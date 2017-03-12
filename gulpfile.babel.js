4269461b590fe977e8f15d222fd2bf0d6758a2a1
Cédric Blondeau <hi@cedricblondeau.com>
2017-03-11 20:51:43 -0500

    Set up basic styles

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,21 @@
 import browserSync from 'browser-sync'
 import gulp from 'gulp'
+import gulpLoadPlugins from 'gulp-load-plugins'
 
-gulp.task('serve', () => {
+const $ = gulpLoadPlugins()
+
+gulp.task('styles', () => {
+  return gulp.src(['app/styles/main.scss'])
+    .pipe($.sass({precision: 10}).on('error', $.sass.logError))
+    .pipe(gulp.dest('.tmp/styles'))
+})
+
+gulp.task('serve', ['styles'], () => {
   browserSync({
-    server: ['app'],
+    server: ['app', '.tmp'],
     port: 3000
   })
 
   gulp.watch(['app/index.html'], [browserSync.reload])
+  gulp.watch(['app/styles/**/*.scss'], ['styles', browserSync.reload])
 })
